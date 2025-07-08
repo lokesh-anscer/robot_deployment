014b96b67be19b2bfe587c372bf620b08aea6450
lokesh-anscer <lokesh@anscer.com>
2025-07-08 15:18:58 +0530

    test

--- setup.sh
+++ setup.sh
@@ -5,7 +5,13 @@ log() {
   echo "[robot-setup] $1"
 }
 
+log "Waiting for Kubernetes API to become available..."
+until kubectl get nodes &>/dev/null; do
+  echo "[robot-setup] Waiting for K3s to be ready..."
+  sleep 3
+done
+
 log "Deploying app manifests via kubectl..."
-kubectl apply -k manifest/
+kubectl apply -k manifests/
 
 log "Setup complete!"
